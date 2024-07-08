4c60533fe7421f79d44292a054463a9e0c74a11d
Theo <tpingouin@gmail.com>
2024-07-08 22:42:21 +0900

    window commit

---
+++ ROKEY bootcamp/Python_Exercise/test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
+
+print("✳✳✳✳✳✳✳✳✳✳✳✳")
+print("✳✳✳✳✳✳✳✳✳✳✳✳")
+print("✳✳✳✳✳✳✳✳✳✳✳✳")
+print("✳✳   ✳✳✳✳   ✳✳")
+print("✳✳✳   ✳✳✳   ✳✳✳")
+print("✳✳✳✳   ✳   ✳✳✳✳")
+print("✳✳✳✳✳   ✳✳✳✳✳")
+print("✳✳✳✳✳   ✳✳✳✳✳")
+print("✳✳✳✳✳   ✳✳✳✳✳")
+print("✳✳✳✳✳✳✳✳✳✳✳✳")
+print("✳✳✳✳✳✳✳✳✳✳✳✳")
