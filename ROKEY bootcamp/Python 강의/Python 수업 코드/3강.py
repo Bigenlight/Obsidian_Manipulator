599b98f833c8424acb8f7e626035b3732d1260e6
Theo <tpingouin@gmail.com>
2024-07-10 12:31:11 +0900

    window commit

---
+++ ROKEY bootcamp/Python 강의/Python 수업 코드/3강.py
@@ -0,0 +1,43 @@
+a = 5
+b = a
+
+a += 1
+
+print(b)
+
+
+print("area: %s" % "Baldur's Gate") 
+print("temp: %d" % 10)
+
+print("area: %s, temp: %d, damage: %d" % ("Baldur's Gate", 18, 23 * 2 )) 
+
+print("%10s" % "last")
+print("%10d" % 10)
+print("%10f" % 2.2)
+
+print("%d%%" % 1)
+
+#input("Calculator \nPlease type an 1~100 number \nInput: ")
+
+
+print("%d" % 1.1)
+print("%.1f" % 1)
+
+
+print("%.1f" % 1.05)
+print("%.1f" % 1.04)
+print("%.0f" % 1.6)
+
+x1 = 1.23
+x2 = 12.3
+x3 = 123.456
+
+# 소수점 기준으로 정렬된 출력
+print(f"{x1:8.3f}")
+print(f"{x2:8.3f}")
+print(f"{x3:8.3f}")
+
+x1 = 1.23; x2 = 12.3; x3 = 123.456
+print(f"{x1:10.3f}\n{x2:10.3f}\n{x3:10.3f}")
+
+print(f"\{{}} ")
---
+++ ROKEY bootcamp/Python 강의/Python 이론.md
@@ -0,0 +1,3 @@
+[[Python 1강]]
+[[Python 2강]]
+[[Python 3강]]
