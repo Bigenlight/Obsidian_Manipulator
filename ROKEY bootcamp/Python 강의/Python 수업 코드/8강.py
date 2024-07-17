5326016e25890030813544e15c17c4e3ec9343fb
Bigenlight <tpingouin@gmail.com>
2024-07-17 16:33:12 +0900

    Ubuntu Auto commit

--- ROKEY bootcamp/Python 강의/Python 수업 코드/8강.py
+++ ROKEY bootcamp/Python 강의/Python 수업 코드/8강.py
@@ -4,3 +4,15 @@ c = (a + b)
 print(c)
 c.sort()
 print(c)
+
+import random
+
+l = [random.randint(1,99) for _ in range(10)]
+print(l)
+t = tuple(l)
+print(t)
+l = sorted(t)
+
+print(sum(l))
+print(min(l))
+print(max(l))
